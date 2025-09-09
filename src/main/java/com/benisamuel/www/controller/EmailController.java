13cec62e26feff4caad8543a396d6fd16bfdfa75
benisamuel <benisamuel566@gmail.com>
2025-09-09 22:00:29 +0200

    added email and user controller

--- src/main/java/com/benisamuel/www/controller/EmailController.java
+++ src/main/java/com/benisamuel/www/controller/EmailController.java
@@ -1,4 +1,24 @@
 package com.benisamuel.www.controller;
 
+import com.benisamuel.www.service.EmailService;
+import com.benisamuel.www.util.ApiResponse;
+import jakarta.mail.MessagingException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/smis/email")
+@AllArgsConstructor
 public class EmailController {
+    private final EmailService emailService;
+
+    @GetMapping("/report-generated")
+    public ResponseEntity<ApiResponse<Boolean>> generateReportMails () throws MessagingException {
+        emailService.sendReportedGeneratedMails("ishimwebeni544@gmail.com","Report Generated", "Beni Samuel");
+        return ApiResponse.ok("Generated Reports!!!", true);
+
+    }
 }
--- src/main/java/com/benisamuel/www/controller/UserController.java
+++ src/main/java/com/benisamuel/www/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.benisamuel.www.controller;
 
+import com.benisamuel.www.dto.UserDto;
+import com.benisamuel.www.model.User;
+import com.benisamuel.www.service.UserService;
+import com.benisamuel.www.util.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/smis/user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<List<User>>> getAllUsers () {
+        return ApiResponse.ok("Successfully obtained all user!!!", userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<User>> getUserById (@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ApiResponse.ok("Successfully obtained user!!!", user);
+        }
+        return ApiResponse.notFound("Failed to obtain user!!!", null);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<User>> createUser (@Valid @RequestBody UserDto userDto) {
+        User newUser = userService.createUser(userDto);
+        if (newUser != null) {
+            return ApiResponse.created("Successfully created a user!!!", newUser);
+        }
+        return ApiResponse.badRequest("Failed to create a user!!!", null);
+    }
 }
