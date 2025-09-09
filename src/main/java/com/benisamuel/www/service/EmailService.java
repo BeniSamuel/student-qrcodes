d17fb4088d755f677d03d8cea8600180e28d5ffd
benisamuel <benisamuel566@gmail.com>
2025-09-09 22:02:43 +0200

    added email and user service

--- src/main/java/com/benisamuel/www/service/EmailService.java
+++ src/main/java/com/benisamuel/www/service/EmailService.java
@@ -1,4 +1,32 @@
 package com.benisamuel.www.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public void sendReportedGeneratedMails (String to, String subject, String name) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setFrom("benisamuel566@gmail.com");
+
+        // Email body with HTML
+        String content = "<h2>Hello, " + name + "</h2>"
+                + "<p>We are happy to inform you that: </p>"
+                + "<p>That reports are now generated!!!</p>";
+
+        helper.setText(content, true);
+        javaMailSender.send(mimeMessage);
+    }
+
 }
--- src/main/java/com/benisamuel/www/service/UserService.java
+++ src/main/java/com/benisamuel/www/service/UserService.java
@@ -1,4 +1,38 @@
 package com.benisamuel.www.service;
 
+import com.benisamuel.www.dto.UserDto;
+import com.benisamuel.www.model.User;
+import com.benisamuel.www.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers () {
+        return userRepository.findAll();
+    }
+
+    public User getUserById (Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByEmail (String email) {
+        return userRepository.getUserByEmail(email);
+    }
+
+    public User createUser (UserDto userDto) {
+        User user = getUserByEmail(userDto.getEmail());
+        if (user != null) {
+            return null;
+        }
+        User newUser = new User(userDto.getName(), userDto.getEmail(), userDto.getPassword(), userDto.getRole());
+        return userRepository.save(newUser);
+    }
+
+
 }
