a0bc638194dc17950e6dfb29c617f07e4402d9bf
benisamuel <benisamuel566@gmail.com>
2025-09-09 22:00:54 +0200

    added userdto

--- src/main/java/com/benisamuel/www/dto/UserDto.java
+++ src/main/java/com/benisamuel/www/dto/UserDto.java
@@ -1,4 +1,22 @@
 package com.benisamuel.www.dto;
 
+import com.benisamuel.www.enums.Role;
+import jakarta.validation.constraints.*;
+
+import lombok.Getter;
+
+@Getter
 public class UserDto {
+    @NotBlank(message = "name can't be blank")
+    private String name;
+
+    @NotBlank(message = "email field can't be empty")
+    @Email(message = "use a standard email format")
+    private String email;
+
+    @Size(max = 8, message = "password can be at least of 8 character")
+    private String password;
+
+    @NotBlank(message = "role field can't be empty")
+    private Role role;
 }
